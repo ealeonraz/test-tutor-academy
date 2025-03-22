30e4d54dc7382d8860f45bbbad1be946895dc00c
Julio Escobar <vita.escobar01@gmail.com>
2025-03-22 10:08:44 -0700

    (Jira #87) Minoring updating Student Dashboard Page with minor deletes of code and formatting

--- client/src/pages/studentDashboard.jsx
+++ client/src/pages/studentDashboard.jsx
@@ -3,6 +3,8 @@ import Footer from '../components/Footer';
 import StudentDashboardNavbar from '../components/dashboardNavbar';
 import Navbar from '../components/Navbar';
 
+import './studentDashboard.css';
+
 function StudentDashboardHome(){
     return(
         <div className= "student-dashboard-container">
@@ -10,7 +12,7 @@ function StudentDashboardHome(){
             <StudentDashboardNavbar />
             <div className = "student-dashboard-content">
                 <div className ="student-welcome-box">
-
+                    
                 </div>
                 <h1>Welcoming the user to their dashboard</h1>
             </div>
